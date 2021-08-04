502633ef0dc894c06f8e762b23b3dacaedb9f2f6
Florian Nücke <florian@nuecke.de>
2021-08-04 19:47:56 +0200

    Update gradle, fix artifact version for maven.

--- src/main/java/li/cil/oc2/sedna/OC2Sedna.java
+++
@@ -1,7 +0,0 @@
-package li.cil.oc2.sedna;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod("oc2-sedna")
-public final class OC2Sedna {
-}
---
+++ src/main/java/li/cil/sednamc/Sedna.java
@@ -0,0 +1,7 @@
+package li.cil.sednamc;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("sedna")
+public final class Sedna {
+}
